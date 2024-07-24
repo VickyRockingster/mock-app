08651d420c1ef0cd5f81a3f5daaf3d4190efff2c
Victorya Rockingster <vicky@Victoryas-MacBook-Pro.local>
2024-07-24 02:51:21 -0400

    Set up basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import './App.css';
-import logo from './logo.svg';
+import HomePage from './components/home/HomePage';
+import GuidesPage from './components/connections/guides/GuidesPage';
+import LifeLessonsPage from './components/connections/lifeLessons/LifeLessonsPage';
+import { Route, Routes } from 'react-router-dom';
+import { LegalPage, LovedOnesPage, PersonalInfoPage } from './components/otherPages'
+import { LettersHomePage, LetterPage, NewLetterPage } from './components/connections/lettersHome/LettersHomePage';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/personal-info' element={<PersonalInfoPage />} />
+				<Route path='/loved-ones' element={<LovedOnesPage />} />
+				<Route path='/legal' element={<LegalPage />} />
+
+				<Route path='/letters-home' element={<LettersHomePage />}>
+					<Route path='new' element={<NewLetterPage />} />
+					<Route path=':letterHomeId' element={<LetterPage />} />
+				</Route>
+
+				<Route path='/life-lessons' element={<LifeLessonsPage />} />
+				<Route path='/guides' element={<GuidesPage />} />
+
+				
+				<Route path='*' element={<h1>Page Not Found</h1>} />
+			</Routes>
     </div>
   );
 }
-
-export default App;
