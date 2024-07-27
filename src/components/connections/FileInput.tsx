87e03cbef881a5cfd29ceb5612530f61cca42b2f
Victorya Rockingster <vicky@Victoryas-MacBook-Pro.local>
2024-07-27 00:33:25 -0400

    Finish styling and logic to upload images

--- src/components/connections/FileInput.tsx
+++ src/components/connections/FileInput.tsx
@@ -1,48 +1,47 @@
-import { Fragment, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import axios from "axios"
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function FileInput() {
-	const [fileInputValue, setFileInputValue] = useState('');
-	const [fileValue, setFileValue] = useState<File | null>(null);
+	const [fileInputValue, setFileInputValue] = useState('')
+	const [fileValue, setFileValue] = useState<File | null>(null)
 
 	function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-		event.preventDefault();
-		console.log('event.target.value from file upload:', event.target.value)
-		console.log('event.target.files from file upload:', event.target.files)
-    setFileInputValue(event.target.value);
+		event.preventDefault()
+    setFileInputValue(event.target.value)
 		if (event.target.files) {
 			setFileValue(event.target.files[0])
 		}
-  };
+  }
 
 	const location = useLocation()
 	const resourcePath = location.pathname.split('/')[1]
-	// console.log('fileInputValue from file upload:', fileInputValue)
-	function handleSubmit(event: React.FormEvent<HTMLButtonElement>) {
-		event.preventDefault();
 
-		const formData: any = new FormData();
-		formData.append('name', fileInputValue);
-		formData.append('file', fileValue);
-		console.log('formData from submit file:', formData)
+	const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
 
-		fetch(`http://localhost:8000/api/upload/${resourcePath}`, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: formData,
-		})
-			.then(response => {
-				if (!response.ok) {
-					throw new Error('Network response was not ok');
-				}
-				return response.json();
-			})
-			.then(writtenData => console.log('file Data from POST:', writtenData))
-			.catch(error => console.error('Error:', error));
+		// Do not submit if there's no file
+		if (!fileInputValue || !fileValue) {
+			return
+		}
+		const formData: any = new FormData()
+		formData.append('name', fileInputValue)
+		formData.append('file', fileValue)
+		try {
+			await axios.post(
+			`http://localhost:8000/api/upload/${resourcePath}/`,
+			formData,
+			{ headers: {'Content-Type': 'multipart/form-data'}})
+		} catch(error) {
+			console.error('Error:', error)
+		}
+		//Reset form after submission
+		setFileInputValue('')
+		setFileValue(null)
 	}
 
 	return (
-		<form className='file-upload-form'>
+		<form className='file-upload-form' onSubmit={handleSubmit}>
 			<label className='visually-hidden' htmlFor='file-upload'>Upload</label>
 			<input
 			className='file-upload-input'
@@ -50,17 +49,17 @@ export default function FileInput() {
 			name='file-upload'
 			id='file-upload'
 			type='file'
-			value={ fileInputValue }
+			value={fileInputValue}
 			onChange={handleInputChange}
 			>
 			</input>
 			{ 
-			fileInputValue ? 
-				<Fragment>
-					<div className='file-name'>{ fileInputValue }</div>
-					<button type='submit' onSubmit={handleSubmit}>Submit</button> 
-				</Fragment>
-			: null
+				fileInputValue ? 
+					<>
+						<div className='file-name'>{fileInputValue}</div>
+						<button type='submit'>Submit</button> 
+					</>
+				: null
 			}
 		</form>
 	)
