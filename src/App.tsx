9deb9a94a9d69272d438119548ed7c0f280f0f98
System Administrator <root@Victoryas-MacBook-Pro.local>
2024-07-28 15:39:30 -0400

    Fix routing for live site on gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { LifeLessonsPage, NewLifeLessonPage } from './components/connections/lif
 import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
+
   return (
     <div className="App">
 			<Routes>
